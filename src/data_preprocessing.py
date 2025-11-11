f65e24c8e762fb2b463fa80246dbab7ba36fcea4
arshdeep6822 <deeparsh618@gmail.com>
2025-11-11 21:29:20 +0530

    test case with with params 1 and their files saved

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -8,7 +8,7 @@ import string
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
-
+nltk.download('punkt_tab')
 # Ensure the "logs" directory exists
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
